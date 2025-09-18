1fec1438f958a57f78b1ebe67bfb4b4703a4dd84
Xurshid Ismoilov <khurshidi2827@gmail.com>
2025-09-18 12:51:21 +0500

    >

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.enableCors(); // CORS ni yoqish
 
   await app.listen(55422);
 }
