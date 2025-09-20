9f2c74842d29369d3218c6e903a35c073ccec5c6
Xurshid Ismoilov <khurshidi2827@gmail.com>
2025-09-20 19:05:18 +0500

    web version

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [PrismaModule, CategoryModule],
+  imports: [PrismaModule, CategoryModule, ServeStaticModule.forRoot({
+    rootPath: join(__dirname,"..", 'web'),
+    serveRoot: '/view', 
+    renderPath: '/view*',
+  
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1' ,{
+    exclude: ['/view', '/view/(.*)'],
+  });
   app.enableCors(); // CORS ni yoqish
 
   await app.listen(55422);
+  console.log('API 👉 http://localhost:55422/api/v1');
+  console.log('WEB 👉 http://localhost:55422/view');
 }
 bootstrap();
