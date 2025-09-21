4898bff6d3351925ab57e42f00fe96e22d81eaa7
Xurshid Ismoilov <khurshidi2827@gmail.com>
2025-09-22 02:27:59 +0500

    >

--- src/jwt/user.decoration.ts
+++ src/jwt/user.decoration.ts
@@ -4,6 +4,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+    console.log("request.user : ",request.user);
+    
     return request.user; // JwtStrategy.validate() dan keladi
   },
 );
